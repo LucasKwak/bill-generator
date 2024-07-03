2fb71a6609a57412197d1919dee28aa45037723e
LucasKwak <lkwakiglesia@gmail.com>
2024-07-03 20:24:08 +0200

    [COMP-2]: Add structure and logic of ItemsList. Communications between father and child (ItemGenerator) are established.

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="main-wrapper">
         <section>
-            <ItemGenerator></ItemGenerator>
+            <ItemGenerator @addItem="añadirItem"></ItemGenerator>
         </section>
     </div>
 </template>
 
 <script lang="ts" setup>
+    import { Ref, ref } from "vue";
     import ItemGenerator from './ItemGenerator.vue';
+
+    interface IItem {
+        name :string,
+        category :string,
+        price :number,
+        units :number
+    }
+
+    let items:Ref<Array<IItem>> = ref([]);
+
+    function añadirItem(item:IItem) {
+        items.value.push(item);
+    }
 
 </script>
 
